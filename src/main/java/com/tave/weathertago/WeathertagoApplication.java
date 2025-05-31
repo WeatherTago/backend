792c3b934c028a536751fca591fb78b9f868baff
jiwon <ysjjw2003@gmail.com>
2025-05-31 13:02:30 +0900

    feat: add new files

---
+++ src/main/java/com/tave/weathertago/WeathertagoApplication.java
@@ -0,0 +1,13 @@
+package com.tave.weathertago;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WeathertagoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WeathertagoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tave/weathertago/WeathertagoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tave.weathertago;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WeathertagoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
