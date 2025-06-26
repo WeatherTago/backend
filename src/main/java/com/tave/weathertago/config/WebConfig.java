4a48318460f927bfed1201db6d68c06b5c6e0aba
jiwon <ysjjw2003@gmail.com>
2025-06-26 23:56:31 +0900

    fix: fix webconfig

--- src/main/java/com/tave/weathertago/config/WebConfig.java
+++ src/main/java/com/tave/weathertago/config/WebConfig.java
@@ -23,7 +23,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("https://jionly.tech")
+                .allowedOrigins("https://www.jionly.tech") // 프론트엔드 도메인
                 .allowedOriginPatterns("*") // “*“같은 와일드카드를 사용
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "PATCH") // 허용할 HTTP method
                 .allowCredentials(true) // 쿠키 인증 요청 허용
