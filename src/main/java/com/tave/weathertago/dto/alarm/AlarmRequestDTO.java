436cf794526cc9df1e2384b59a461f045a9fa4fb
jiwon <ysjjw2003@gmail.com>
2025-06-29 18:01:47 +0900

    feat: alarm command service

--- src/main/java/com/tave/weathertago/dto/alarm/AlarmRequestDTO.java
+++ src/main/java/com/tave/weathertago/dto/alarm/AlarmRequestDTO.java
@@ -1,4 +1,21 @@
 package com.tave.weathertago.dto.alarm;
 
+import com.tave.weathertago.domain.AlarmDay;
+import com.tave.weathertago.domain.Station;
+import lombok.Builder;
+import lombok.Getter;
+
+import java.time.LocalTime;
+
 public class AlarmRequestDTO {
+
+    @Getter
+    @Builder
+    public static class AlarmCreateRequestDTO {
+        private String pushToken;
+        private LocalTime referenceTime;
+        private Station stationName;
+        private AlarmDay alarmDay;
+        private LocalTime alarmTime;
+    }
 }
--- src/main/java/com/tave/weathertago/service/alarm/AlarmCommandService.java
+++ src/main/java/com/tave/weathertago/service/alarm/AlarmCommandService.java
@@ -1,7 +1,13 @@
 package com.tave.weathertago.service.alarm;
 
 import com.tave.weathertago.dto.alarm.AlarmFcmMessageDto;
+import com.tave.weathertago.dto.alarm.AlarmRequestDTO;
 
 public interface AlarmCommandService {
     void sendAlarm(AlarmFcmMessageDto dto);
+
+    Long createAlarm(Long userId, AlarmRequestDTO.AlarmCreateRequestDTO dto);
+    void updateAlarm(Long alarmId, AlarmRequestDTO.AlarmCreateRequestDTO dto);
+    void sendAlarm();
+    void deleteAlarm(Long alarmId);
 }
