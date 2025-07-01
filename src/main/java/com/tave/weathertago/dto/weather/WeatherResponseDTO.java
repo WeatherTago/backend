55e14779153490ba6c91babb4a9af61a298cf6cd
yujeong <parkyj0430@gmail.com>
2025-07-02 08:22:21 +0900

    feat: 기상청 API 받아와서 날씨 시간대별로 redis 저장 구현

---
+++ src/main/java/com/tave/weathertago/dto/weather/WeatherResponseDTO.java
@@ -0,0 +1,21 @@
+package com.tave.weathertago.dto.weather;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.io.Serializable;
+
+@Getter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class WeatherResponseDTO implements Serializable {
+    private double tmp;
+    private double reh;
+    private double pcp;
+    private double wsd;
+    private double sno;
+    private double vec;
+}
