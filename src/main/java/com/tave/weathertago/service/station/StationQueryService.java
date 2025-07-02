2146d5ff82475674c502bcc95ea81aa58b44cad5
wonjun-lee-fcwj245 <fcwj24@gail.com>
2025-07-02 23:06:32 +0900

    fix: stash 복원 중 충돌 해결 및 station 디렉토리 정리

---
+++ src/main/java/com/tave/weathertago/service/station/StationQueryService.java
@@ -0,0 +1,19 @@
+package com.tave.weathertago.service.station;
+
+import com.tave.weathertago.dto.station.StationResponseDTO;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+public interface StationQueryService {
+    List<StationResponseDTO.JoinResultDTO> getStationsByName(String name, LocalDateTime time);
+
+
+    StationResponseDTO.JoinResultDTO getStationByNameAndLine(String name, String line, LocalDateTime time);
+
+
+
+    /*
+    String getStationCodeByNameAndLine(String name, String line);
+     */
+}
