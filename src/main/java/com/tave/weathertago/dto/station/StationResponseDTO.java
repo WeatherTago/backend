36493da2ec8363481e4c40981978b6963a2ab07a
wonjun-lee-fcwj245 <fcwj24@gail.com>
2025-07-08 22:45:13 +0900

    feat:106-modify-cratedAt

--- src/main/java/com/tave/weathertago/dto/station/StationResponseDTO.java
+++ src/main/java/com/tave/weathertago/dto/station/StationResponseDTO.java
@@ -1,7 +1,9 @@
 package com.tave.weathertago.dto.station;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import com.tave.weathertago.dto.CongestionDTO;
 import com.tave.weathertago.dto.weather.WeatherResponseDTO;
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -21,6 +23,8 @@ public class StationResponseDTO {
         String stationCode;
         WeatherResponseDTO weather;
         CongestionDTO congestion;
+        @Schema(type = "string", format = "date-time", example = "2025-07-08T22:40:00")
+        @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd'T'HH:mm:ss", timezone = "Asia/Seoul")
         LocalDateTime createdAt;
     }
 
